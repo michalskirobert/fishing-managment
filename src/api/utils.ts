939679c0915bd14c4f004aedf20031222d4011f5
Robert <majointa@gmail.com>
2024-07-28 13:58:57 +0200

    fix version

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,11 +1,13 @@
 export const processApiUrl = () => {
   if (typeof window !== "undefined") {
-    const hostname = location.hostname.split(".")[0];
-    const domain = location.hostname.split(".").slice(1).join(".");
+    // const hostname = location.hostname.split(".")[0];
+    // const domain = location.hostname.split(".").slice(1).join(".");
+
+    // const API = `https://${hostname}-api.${domain}`;
 
     return process.env.NODE_ENV === "development"
       ? "http://localhost:3001"
-      : `https://${hostname}-api.${domain}`;
+      : "https://fishing-app-api.onrender.com";
   } else {
     return "";
   }
